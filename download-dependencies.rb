2c87cb45eb91d27bcc691568c19524ead4c34118
drrb <drrrrrrrrrrrb@gmail.com>
2014-05-04 21:24:03 +1000

    Changed name to Go Puppet Forge Poller

--- download-dependencies.rb
+++ download-dependencies.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 #
-# Go Forge Poller
+# Go Puppet Forge Poller
 # Copyright (C) 2014 drrb
 #
 # This program is free software: you can redistribute it and/or modify
@@ -14,7 +14,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Go Forge Poller. If not, see <http://www.gnu.org/licenses/>.
+# along with Go Puppet Forge Poller. If not, see <http://www.gnu.org/licenses/>.
 #
 
 require 'fileutils'
